7611b726e898897f7cf744ccc6ecea9fff3b6582
DWJ-LDT\SSSC <kwjang97@gmail.com>
2022-02-08 23:50:05 +0900

    Implemented argsparser; python train.py -cfg ./data/distiller/~~.yaml

--- train.py
+++ train.py
@@ -1,5 +1,10 @@
 import os
 import re
+import time
+from datetime import datetime
+from pytz import timezone
+import yaml
+import argparse
 import logging
 import numpy as np
 import torch
@@ -9,7 +14,9 @@ from torch.utils.data import Dataset, DataLoader
 from datasets import load_metric
 from transformers import get_scheduler
 
-from utils import *
+from utils.utils import *
+from utils.dataset import *
+from utils.utils_for_custom import dump_yaml
 from modules.model import CustomStudentModelConfig, CustomStudentModel
 
 from importlib import reload
@@ -20,9 +27,21 @@ from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
+
 # CONFIG ------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', '-cfg',
+                    required = True,
+                    help= 'config for training')
+YAML_PATH = parser.parse_args().config
+with open(YAML_PATH) as f:
+    YAML_CFG = yaml.load(f, Loader = yaml.FullLoader)
+
+BATCH_SIZE = YAML_CFG['train']['batch_size']
+# -------------------------------------
+'''
 # Teacher model related
-TEACHER_MODEL = 'wav2vec_small.pt'
+TEACHER_MODEL = '../parameters/w2v2/wav2vec_small.pt'
 
 # Student model related
 STUDENT_ENCODER_LAYERS = 2
@@ -64,13 +83,16 @@ TEST = False
 TEST_SET = "test-clean"
 # TEST_SET = "test-other"
 # --------------------------------------
+'''
 
 class W2V2Distil(LightningModule):
-    def __init__(self, 
-                data_path=DATA_PATH,
-                batch_size=BATCH_SIZE,
+    def __init__(self,
+                 yaml_cfg = YAML_CFG,
+                 batch_size = BATCH_SIZE,
                 ):
         super().__init__()
+
+        self.yaml_cfg = yaml_cfg
 
         self.save_hyperparameters()
 
@@ -88,18 +110,30 @@ class W2V2Distil(LightningModule):
             "F": 20, "G": 21, "Y": 22, "P": 23, "B": 24, "V": 25, "K": 26, 
             "'": 27, "X": 28, "J": 29, "Q": 30, "Z": 31}
 
-        self.teacher_model, teacher_config, self.task_agnostic = load_model_and_config(TEACHER_MODEL)
+        # Load teacher model
+        teacher_model = self.yaml_cfg['teacher']['teacher_model']
+        self.teacher_model, teacher_config, self.task_agnostic = load_model_and_config(teacher_model)
         self.teacher_model.encoder.layerdrop = 0
         freeze_model(self.teacher_model)
 
-        # Assign more configs about student model compares to teacher model
+        # Assign more configs about student compares to teacher
         student_config = CustomStudentModelConfig()
         target_keys = student_config._get_all_attributes()
         for k in teacher_config.keys():
             if k in target_keys:
                 setattr(student_config, k, getattr(teacher_config, k))
-        self.student_config = student_config       
-        self.update_student_config()
+
+        # Only several attributes referred in this method are updated to student
+        # even though you write something about student model in config yaml file 
+        self.student_config = student_config
+        distiller_cfg = self.yaml_cfg['distiller']
+        self.update_student_config(distiller_cfg)
+
+        data_cfg = self.yaml_cfg['data']
+        bucketing_path = data_cfg['bucketing_path']
+        libri_root = data_cfg['libri_root']
+        train_set = data_cfg['train_set']
+        test_set =data_cfg['test_set']
 
         # Model Initialize -> Distillation training -> Add FC/Dropout & Fine-tuning
         self.student_model = CustomStudentModel(
@@ -109,23 +143,25 @@ class W2V2Distil(LightningModule):
 
         # download & prepare data
         self.train_data = LibriDataset(
-            batch_size=BATCH_SIZE,
-            file_path=DATA_PATH,
-            sets=DATA_SETS,
-            libri_root=LIBRI_ROOT,
+            batch_size=batch_size,
+            file_path=bucketing_path,
+            sets=train_set,
+            libri_root=libri_root,
         )
         self.eval_data = LibriDataset(
-            batch_size=BATCH_SIZE,
-            file_path=DATA_PATH,
+            batch_size=batch_size,
+            file_path=bucketing_path,
             sets=['dev-clean'],
-            libri_root=LIBRI_ROOT,
+            libri_root=libri_root,
         )
         self.test_data = LibriDataset(
-            batch_size=BATCH_SIZE,
-            file_path=DATA_PATH,
-            sets=[TEST_SET],
-            libri_root=LIBRI_ROOT,
+            batch_size=batch_size,
+            file_path=bucketing_path,
+            sets=test_set,
+            libri_root=libri_root,
         )
+
+        dump_yaml(self.student_config, self.yaml_cfg)
 
         # For better pytorch lightning logging
         logging.shutdown()
@@ -190,7 +226,7 @@ class W2V2Distil(LightningModule):
             
         rec_loss = rec_loss.mean()
         
-        if COSINE_LOSS_WEIGHT > 0:
+        if self.yaml_cfg['train']['cosine_weight'] > 0:
             sim_loss = -F.logsigmoid(F.cosine_similarity(proj, target, dim=-1))
             with torch.no_grad():
                 sim_layer_loss = sim_loss.mean((0, 2))
@@ -199,7 +235,7 @@ class W2V2Distil(LightningModule):
             sim_loss = 0
             sim_layer_loss = None
             
-        total_loss = rec_loss + COSINE_LOSS_WEIGHT * sim_loss
+        total_loss = rec_loss + self.yaml_cfg['train']['cosine_weight'] * sim_loss
         
         losses = torch.add(rec_layer_loss, sim_layer_loss)
         
@@ -209,7 +245,7 @@ class W2V2Distil(LightningModule):
             # Process output for CTC loss
             ctc_input = student_results['x'].log_softmax(2) # -> Revise this
 
-            if USE_GT_FOR_CTC:
+            if self.yaml_cfg['train']['use_gt_for_ctc']:
                 # Use Ground Truth labels instead of labels from the teacher model
                 gt_tokens = [torch.tensor([self.char_dict[char] for char in label]) for label in labels]
                 target = torch.cat(gt_tokens)
@@ -232,23 +268,24 @@ class W2V2Distil(LightningModule):
 
         return total_loss, losses
 
-    def update_student_config(self):
+    def update_student_config(self, cfg: dict):
         # Set student w2v model configs before distillation
+        # These attributes are not dependent to teacher model
 
         # Model spec related
-        self.student_config.encoder_layers = STUDENT_ENCODER_LAYERS
-        self.student_config.enable_tr_layer = ENABLE_TR_LAYER
-        self.student_config.type_of_tr_layer = TR_TYPE
-        self.student_config.tr_layer_floor = TR_LAYER_FLOOR
+        self.student_config.encoder_layers = cfg['encoder_layers']
+        self.student_config.enable_tr_layer = cfg['enable_tr_layer']
+        self.student_config.type_of_tr_layer = cfg['type_of_tr_layer']
+        self.student_config.tr_layer_floor = cfg['tr_layer_index']
         
         # Initialization related
-        self.student_config.init_conv_layers = INIT_TEACHER_CONV_LAYERS
-        self.student_config.init_encoder_layers = INIT_TEACHER_ENCODER_LAYERS
+        self.student_config.init_conv_layers = cfg['init_conv_layers']
+        self.student_config.init_encoder_layers = cfg['init_encoder_layers']
 
         # Prediction head related
-        self.student_config.proj_head_inter_dim = PROJ_HEAD_INTER_DIM
-        self.student_config.proj_head_final_dim = PROJ_HEAD_FINAL_DIM
-        self.student_config.pred_layer_id = PRED_LAYER_ID
+        self.student_config.proj_head_inter_dim = cfg['pred_head_inter_dim']
+        self.student_config.proj_head_final_dim = cfg['pred_head_final_dim']
+        self.student_config.pred_layer_id = cfg['pred_layer_id']
         self.student_config.teacher_task_agnostic = self.task_agnostic
 
 
@@ -260,7 +297,7 @@ class W2V2Distil(LightningModule):
         else:
             loss, losses = self.calculate_loss(student_results, teacher_results)
 
-        if MONITOR_LOSSES:
+        if self.yaml_cfg['train']['monitor_losses']:
             for i, l in enumerate(losses):
                 self.log(f"loss{i+1}", l.item(), prog_bar=True)
 
@@ -322,12 +359,12 @@ class W2V2Distil(LightningModule):
             self.log("cer", cer, on_epoch=True, prog_bar=True, batch_size=self.hparams.batch_size)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=LEARNING_RATE)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.yaml_cfg['train']['learning_rate'])
         # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=8, factor=0.1, verbose=True)
 
-        train_batches = len(self.train_dataloader()) // GPUS
-        num_training_steps = (NUM_EPOCHS * train_batches) // ACCUMULATE_GRAD_BATCHES
-        num_warmup_steps = int(num_training_steps * WARMUP_PROPORTION)
+        train_batches = len(self.train_dataloader()) // self.yaml_cfg['train']['gpus']
+        num_training_steps = (self.yaml_cfg['train']['num_epochs'] * train_batches) // self.yaml_cfg['train']['accumulate_grad_batches']
+        num_warmup_steps = int(num_training_steps * self.yaml_cfg['train']['warmup_ratio'])
 
         lr_scheduler = get_scheduler(
             "linear",
@@ -345,13 +382,23 @@ class W2V2Distil(LightningModule):
         }
 
     def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=1, shuffle=True, collate_fn=self.train_data.collate_fn, num_workers=GPUS*4)
+        return DataLoader(self.train_data,
+                          batch_size=1,
+                          shuffle=True,
+                          collate_fn=self.train_data.collate_fn,
+                          num_workers=self.yaml_cfg['train']['gpus']*4)
 
     def val_dataloader(self):
-        return DataLoader(self.eval_data, batch_size=1, collate_fn=self.eval_data.collate_fn, num_workers=GPUS*4)
+        return DataLoader(self.eval_data,
+                          batch_size=1,
+                          collate_fn=self.eval_data.collate_fn,
+                          num_workers=self.yaml_cfg['train']['gpus']*4)
     
     def test_dataloader(self):
-        return DataLoader(self.test_data, batch_size=1, collate_fn=self.data_collator, num_workers=GPUS*4)
+        return DataLoader(self.test_data,
+                          batch_size=1,
+                          collate_fn=self.data_collator,
+                          num_workers=self.yaml_cfg['train']['gpus']*4)
 
     def get_progress_bar_dict(self):
         tqdm_dict = super().get_progress_bar_dict()
@@ -359,14 +406,23 @@ class W2V2Distil(LightningModule):
             del tqdm_dict['v_num']
         return tqdm_dict
 
+
 if __name__ == '__main__':
-    if CHECKPOINT:
-        model = W2V2Distil().load_from_checkpoint(OUTPUT_DIR + CHECKPOINT)
+
+    output_dir = YAML_CFG['data']['output_dir']
+    checkpoint = YAML_CFG['data']['checkpoint']
+    gpus = YAML_CFG['train']['gpus']
+    num_epochs = YAML_CFG['train']['num_epochs']
+    accumulate_grad_batches = YAML_CFG['train']['accumulate_grad_batches']
+    test = YAML_CFG['train']['test']
+
+    if checkpoint:
+        model = W2V2Distil().load_from_checkpoint(output_dir + checkpoint)
     else:
         model = W2V2Distil()
 
     checkpoint_callback = ModelCheckpoint(
-        dirpath='./results/'+ OUTPUT_DIR,
+        dirpath='./results/'+ output_dir,
         filename='checkpoint-{epoch:02d}',
         verbose=True,
         save_last=True,
@@ -383,20 +439,19 @@ if __name__ == '__main__':
     )
 
     trainer = Trainer(
-        gpus=GPUS,
+        gpus=gpus,
         strategy="ddp",
         amp_backend="apex",
         amp_level="O2",
         precision=16,
-        max_epochs=NUM_EPOCHS,
+        max_epochs=num_epochs,
         sync_batchnorm=True,
-        accumulate_grad_batches=ACCUMULATE_GRAD_BATCHES,
+        accumulate_grad_batches=accumulate_grad_batches,
         callbacks=[early_stopping, checkpoint_callback],
     )
 
-    if TEST:
+    if test:
         trainer.test(model)
     else:
         trainer.fit(model)
 
-    
